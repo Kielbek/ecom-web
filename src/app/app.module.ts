2bcca7e6ad323ab02073610f3127c4c8b7ae86b1
Kielbek <mkielbasa89@gmail.com>
2024-06-28 13:15:40 +0200

    add desktop menu navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderTopComponent } from './components/header-top/header-top.component';
 import { HeaderMainComponent } from './components/header-main/header-main.component';
+import { DesktopNavigationMenuComponent } from './components/desktop-navigation-menu/desktop-navigation-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderTopComponent,
-    HeaderMainComponent
+    HeaderMainComponent,
+    DesktopNavigationMenuComponent
   ],
   imports: [
     BrowserModule,
